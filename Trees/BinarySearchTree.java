d8fa24a4556670938a9beb8706888e5d14b60c3d
Mimi Chenyao <mimichenyao@gmail.com>
2017-11-01 20:45:52 -0400

    Updated main() method

--- Trees/BinarySearchTree.java
+++ Trees/BinarySearchTree.java
@@ -21,6 +21,18 @@ public class BinarySearchTree {
 		return count;
 	}
 
+	public void rotateRight(Node node) {
+		Node temp = node.left;
+		node.left = temp.right;
+		temp.right = node;
+	}
+
+	public void rotateLeft(Node node) {
+		Node temp = node.right;
+		node.right = temp.left;
+		temp.left = node;
+	}
+
 	public boolean find(int id) {
 
 		Node current = root;
@@ -201,20 +213,20 @@ public class BinarySearchTree {
 	}
 
 	public static void main(String[] args){
-		BinarySearchTree b = new BinarySearchTree();
-		b.insert(3);b.insert(8);
-		b.insert(1);b.insert(4);b.insert(6);b.insert(2);b.insert(10);b.insert(9);
-		b.insert(20);b.insert(25);b.insert(15);b.insert(16);
-		System.out.println("Original Tree : ");
-		b.display(b.root);		
-		System.out.println("");
-		System.out.println("Check whether Node with value 4 exists : " + b.find(4));
-		System.out.println("Delete Node with no children (2) : " + b.delete(2));		
-		b.display(root);
-		System.out.println("\n Delete Node with one child (4) : " + b.delete(4));		
-		b.display(root);
-		System.out.println("\n Delete Node with Two children (10) : " + b.delete(10));		
-		b.display(root);
+
+		BinarySearchTree bst = new BinarySearchTree();
+
+		bst.insert(16);
+		bst.insert(13);
+		bst.insert(19);
+		bst.insert(12);
+		bst.insert(15);
+		bst.insert(18);
+		bst.insert(22);
+
+		bst.display(root);
+
+		bst.rightRotation()
 	}
 }
 
